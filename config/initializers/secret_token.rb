34772993d3638a4040051ef420b3598dc93cd56d
Brian Wayne Liou <brian.liou91@gmail.com>
2014-02-24 10:53:45 -0800

    new files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WarmupApp::Application.config.secret_key_base = 'a52fd093c0fde451e7cfd7fc3f61671368ac345487e344dd61d129d22c6fb6a350f619fccc631f8ec59beef2c3abc5627d087a85196bc0f654a5ecc18358d223'
